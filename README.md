e3090ffeb948e5e15179d11c5317b26341efc907
Mark Bacon <mark.n.bacon.jr@gmail.com>
2022-02-23 19:27:00 -0500

    added initial js and updated css styling on share links at bottom of page

--- README.md
+++ README.md
@@ -7,3 +7,18 @@ Added initial structre of HTML Page
     -Contact buttons at the bottom that will eventually have event listeners
     -Created Images folder for About Me content
     ***********************
+
+Added initial structure of CSS page
+    - created a color palette to use throughout
+    - flexed the nav bar 
+    - styled all text content
+    - created a div to hold the two about me pictures
+    - created new div to flex and hold all projects
+
+    ***********************
+
+Added initial JS
+    - added event listeners to buttons on bottom to alert that we will redirect away from page
+    -added button on top to randomly generate the background color of the page using event listener
+Updated CSS
+    - added flex properties to the buttons on bottom of page
--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const h2 = document.createElement("h2");
 
-h2.textContent = "this was created using JavaScript"
+let colorBtn = document.querySelector(".colorBtn")
+const colors = [1, 2, 3, 4, 5, 6, 7, 9, "a", "b", "c", "d", "e", "f"]
 
-document.querySelector("body").appendChild(h2)
+// Write function to randomly generate bg color
+colorBtn.addEventListener("click", function () {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++){
+        hexColor += colors[getRandomNumber()]
+    }
+    document.body.style.backgroundColor = hexColor
+});
+
+
+
+// Write function to get random number
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+
+// External Contact Buttons
+
+const github = document.getElementById('github');
+const linkedin = document.getElementById('linkedin');
+const email = document.getElementById('email');
+
+github.addEventListener("click", function(){
+    alert("Redirecting to GitHub!")
+});
+
+linkedin.addEventListener("click", function(){
+    alert("Redirecting to LinkedIn!")
+});
+
+email.addEventListener("click", function(){
+    alert("Redirecting to Email!")
+});
+
 
